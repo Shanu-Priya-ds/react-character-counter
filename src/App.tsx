31621af86b2e433b9d14c1039d71ffbf00146ea8
Shanu Priya <priyads12@gmail.com>
2026-04-23 23:06:11 -0400

    imported tailwind to the prject and updated TextInput component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import { TextInput } from './components/TextInput/TextInput'
 
 function App() {
   const [count, setCount] = useState(0)
+ const handleTextChange=()=>{
 
+ }
   return (
     <>
-      
+      <TextInput onTextChange={handleTextChange}></TextInput>
     </>
   )
 }
