fc5345fbf9dad6fe5c99fe0d2774ed5379ce2cd5
Shanu Priya <priyads12@gmail.com>
2026-04-24 23:55:12 -0400

    Modify the component structure based on the requirement. updated logic to count the words based on regex

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
 import StatsDisplay from './components/StatsDisplay/StatsDisplay'
+import CharacterCount from "./components/CharacterCounter/CharacterCounter";
 
 function App() {
   return (
     <>
-      <StatsDisplay/>
+      <CharacterCount minWords={25} maxWords={100}></CharacterCount>
     </>
   )
 }
--- src/components/CharacterCounter/CharacterCounter.tsx
+++ src/components/CharacterCounter/CharacterCounter.tsx
@@ -1,9 +1,53 @@
-import type { CharacterCounterProps } from "../../types";
+import { useState } from "react";
+import type { CharacterCounterProps, TextStats } from "../../types";
+import StatsDisplay from "../StatsDisplay/StatsDisplay";
+import { TextInput } from "../TextInput/TextInput";
 
 
-function CharacterCount({minWords,maxWords,targetReadingTime}:CharacterCounterProps){
-    return(<>
-    <span>Min: {minWords} | Max: {maxWords} | Target Readig Time:{targetReadingTime}</span>
+function CharacterCounter({  }: CharacterCounterProps) {
+
+    const defaultReadingTime: number = 0.00;
+    const [charCount, setCharCount] = useState(0);
+    const [wordCount, setWordCount] = useState(0);
+    const [readingTime, setReadingTime] = useState(defaultReadingTime);
+
+    const handleTextChange = (text: string) => {
+        //calculate word count
+        let wordCount: number = 0;
+        let readingTime: number = defaultReadingTime;
+        if (text.length > 0) {
+            const wordsArray: string[] = text.trim().split(/[\s,.!?]+/);
+            console.log(wordsArray);
+            wordCount = wordsArray.length;
+            readingTime = Number((wordCount / 200).toFixed(2));
+        }
+        else {
+            wordCount = 0;
+            readingTime = defaultReadingTime;
+        }
+        setCharCount(text.length);
+        setWordCount(wordCount);
+        setReadingTime(readingTime);//need to calculate
+
+
+    }
+
+    const textStats: TextStats = {
+        characterCount: charCount,
+        wordCount: wordCount,
+        readingTime: readingTime,
+    }
+
+
+
+    return (<>
+
+        <TextInput onTextChange={handleTextChange}></TextInput>
+        <StatsDisplay stats={textStats} showReadingTime={true} />
+
+        {/* <span className="text-xs text-gray-500">Min: {minWords?minWords:0} | Max: {maxWords?maxWords:0} 
+        {(targetReadingTime && targetReadingTime!= undefined) ? 
+        " | Target Reading Time:" + targetReadingTime : null}</span> */}
     </>);
 }
-export default CharacterCount;
+export default CharacterCounter;
